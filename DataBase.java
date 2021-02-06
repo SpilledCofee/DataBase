4f7a73ba279162a70710184569d5cedc7b66c7a0
daedren30 <76187727+daedren30@users.noreply.github.com>
2021-02-05 17:32:46 -0700

    Initial process using user inputs
    If called by main:
    -gets user inputs for 5 fields
    -validates those inputs with the user
    -either starts the process over or exits module (possibly return to main menu)
    
    If later EntryItem is implemented, can be altered to accept an EntryItem object instead of getting user inputs.
    
    Does not yet write to an external file, as I'm waiting to see how the program will handle the file input and output, but it will be using FileWriter to append to the given CSV.

--- DataBase.java
+++ DataBase.java
@@ -5,7 +5,7 @@
         * With 'createRecord' it will create a new entry with the data file
         * 'lookUpRecord' will proceed to search for the correct data file
         * 'updateRecord' will update the current entry with the data file
-'deleteRecord' will remove the current data entry from the data file
+        * 'deleteRecord' will remove the current data entry from the data file
 */
 
 
@@ -15,9 +15,12 @@ import java.util.*;
 
 public class DataBase {
 
+    // temporarily disabled to allow local testing
+    /*
     public DataBase{
 
     }
+    */
 
 
     // Main method to call other methods
@@ -60,15 +63,91 @@ public class DataBase {
 
         try(BufferedReader br = new BufferedReader((new FileReader("CSV_FILE")))){
             String line;
-            while((line = br.readLine() != null)){
-                String[] values =
+            while (((line = br.readLine()) != null)) {
+                String[] values = null; // null added to allow for testing
             }
         }
-
     }
 
     // Method will create a new entry
     public static void createRecord(){
+
+        // user-defined variables
+        String product_id, quantity, wholesale_cost, sale_price, supplier_id;
+
+        // intialize scanner for user input
+        Scanner user_input = new Scanner(System.in);  // create scanner
+
+        // seperator for user readibility
+        String s = "----------------------------------------"; // separator
+        
+        // loop for user inputs and validation, exits when user confirms entries
+        boolean user_confirmed = false;
+        while (!user_confirmed) {
+            // module header
+            
+            System.out.println(s);
+            System.out.println("Inventory Record Creation");
+            System.out.println(s);
+        
+            // get user inputs for all variables
+            System.out.print("Enter Product ID: ");
+            product_id = user_input.nextLine();
+            System.out.print("Enter Quantity: ");
+            quantity = user_input.nextLine();
+            System.out.print("Enter Wholesale Cost: ");
+            wholesale_cost = user_input.nextLine();
+            System.out.print("Enter Sale Price: ");
+            sale_price = user_input.nextLine();
+            System.out.print("Enter Wholesale Cost: ");
+            supplier_id = user_input.nextLine();
+
+            // check validity with user
+            System.out.println(s);
+            System.out.println("You entered the following values:");
+            System.out.println(s);
+            System.out.printf("%15s %10s %15s %12s %12s\n", "PRODUCT ID", "QUANTITY", "WHOLESALE COST", "SALE PRICE", "SUPPLIER ID");
+            System.out.printf("%15s %10s %15s %12s %12s\n", product_id, quantity, wholesale_cost, sale_price, supplier_id);
+            System.out.println(s);
+            System.out.println("Is this correct?");
+            System.out.print("Type 'yes' to add this record, type 'no' to start over: ");
+            String inp = user_input.nextLine();
+            boolean valid = false;
+            while (!valid) {
+                if (inp.toLowerCase().equals("yes")) {
+                    valid = true;
+                    user_confirmed = true;
+                } else if (inp.toLowerCase().equals("no")) {
+                    valid = true;
+                } else {
+                    System.out.print("Invalid response. Please type 'yes' or 'no': ");
+                    inp = user_input.nextLine();
+                }
+            }
+        }
+        
+        // alert user and get next step
+        System.out.println(s);
+        System.out.println("Entry added to inventory!");
+        System.out.println(s);
+        System.out.println("Do you want to add another entry?");
+        System.out.print("Type 'yes' to add another entry, or 'no' to exit to main menu: ");
+        String inp = user_input.nextLine();
+        boolean valid = false;
+        while (!valid) {
+            if (inp.toLowerCase().equals("yes")) {
+                valid = true;
+                createRecord();
+            } else if (inp.toLowerCase().equals("no")) {
+                valid = true;                                       // possibly direct to main menu later
+            } else {
+                System.out.print("Invalid response. Please type 'yes' or 'no': ");
+                inp = user_input.nextLine();
+            }
+        }
+        
+        // close scanner
+        user_input.close();
 
     }
 
