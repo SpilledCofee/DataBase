6b6e2134a5063fa97311feb43f469668b3a1189d
daedren30 <76187727+daedren30@users.noreply.github.com>
2021-02-11 17:01:22 -0700

    Final push before pull request (hopefully)
    Final push before pull request (hopefully)

--- DataBase.java
+++ DataBase.java
@@ -94,7 +94,7 @@ public class DataBase {
             //the correct if statement to proceed to the methods
             String input = dataBase.console.next();
             if (input.contains("a")) {
-                dataBase.createRecord();
+                dataBase.createRecord(dataBase.console);
             }
             if (input.contains("b")) {
                 dataBase.lookUpRecord();
@@ -116,8 +116,119 @@ public class DataBase {
 
 
     // Method will create a new entry
-    public void createRecord(){
+    public void createRecord(Scanner user_input){
 
+        // user-defined variables
+        String product_id = null, supplier_id = null;
+        int quantity = 0;
+        double wholesale_cost = 0, sale_price = 0;
+
+        // seperator for user readibility
+        String s = "----------------------------------------"; // separator
+        
+        // loop for user inputs and validation, exits when user confirms entries
+        boolean user_confirmed = false;
+        while (!user_confirmed) {
+            
+            // module header
+            System.out.println(s);
+            System.out.println("Create Inventory Record");
+            System.out.println(s);
+        
+            // get user inputs for all variables
+
+            // validate product id
+            System.out.print("Enter Product ID: ");
+            product_id = user_input.next();
+            while ((product_id.length()) != 12) {
+                System.out.println("Product ID must be 12 characters long!");
+                System.out.print("Enter Product ID: ");
+                user_input.next();
+            }
+            
+            // validate quantity
+            System.out.print("Enter Quantity: ");
+            while (!user_input.hasNextInt()) {
+                System.out.println("Quantity must be a whole number!");
+                System.out.print("Enter Quantity: ");
+                user_input.next();
+            }
+            quantity = user_input.nextInt();
+
+            // validate wholesale cost
+            System.out.print("Enter Wholesale Cost: ");
+            while (!user_input.hasNextDouble()) {
+                System.out.println("Wholesale cost must be whole number or decimal!");
+                System.out.print("Enter Wholesale Cost: ");
+                user_input.next();
+            }
+            wholesale_cost = user_input.nextDouble();
+
+            // validate sale price
+            System.out.print("Enter Sale Price: ");
+            while (!user_input.hasNextDouble()) {
+                System.out.println("Sale price must be whole number or decimal!");
+                System.out.print("Enter Sale Price: ");
+                user_input.next();
+            }
+            sale_price = user_input.nextDouble();
+
+            // validate supplier id
+            System.out.print("Enter Supplier ID: ");
+            supplier_id = user_input.next();
+            while ((supplier_id.length()) != 12) {
+                System.out.println("Product ID must be 12 characters long!");
+                System.out.print("Enter Supplier ID: ");
+                user_input.next();
+            }
+
+            // confirm entries with user
+            System.out.println(s);
+            System.out.println("You entered the following values:");
+            System.out.println(s);
+            System.out.printf("%15s %10s %15s %12s %15s\n", "PRODUCT ID", "QUANTITY", "WHOLESALE COST", "SALE PRICE", "SUPPLIER ID");
+            System.out.printf("%15s %10s %15s %12s %15s\n", product_id, quantity, wholesale_cost, sale_price, supplier_id);
+            System.out.println(s);
+            System.out.println("Is this correct?");
+            System.out.print("Type 'yes' to add this record, type 'no' to start over: ");
+            String inp = user_input.nextLine();
+            boolean valid = false;
+            while (!valid) {
+                if (inp.toLowerCase().equals("yes")) {
+                    valid = true;
+                    user_confirmed = true;
+                } else if (inp.toLowerCase().equals("no")) {
+                    valid = true;
+                } else {
+                    System.out.print("Invalid response. Please type 'yes' or 'no': ");
+                    inp = user_input.nextLine();
+                }
+            }
+        }
+
+        // create EntryItem object with user inputs
+        EntryItem newItem = new EntryItem(product_id, quantity, wholesale_cost, sale_price, supplier_id);
+        records.add(newItem);
+        
+        // alert user and get next step
+        System.out.println(s);
+        System.out.println("Entry added to inventory!");
+        System.out.println(s);
+        System.out.println("Do you want to add another entry?");
+        System.out.print("Type 'yes' to add another entry, or 'no' to exit to main menu: ");
+        String inp = user_input.nextLine();
+        boolean valid = false;
+        while (!valid) {
+            if (inp.toLowerCase().equals("yes")) {
+                valid = true;
+                createRecord(user_input);
+            } else if (inp.toLowerCase().equals("no")) {
+                valid = true;                                       // possibly direct to main menu later
+            } else {
+                System.out.print("Invalid response. Please type 'yes' or 'no': ");
+                inp = user_input.nextLine();
+            }
+        }
     }
 
 
