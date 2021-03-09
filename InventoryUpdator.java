8500f2e8734215743068e7df0b196bcb8eb63125
Noemi Marquez <njupiter000@gmail.com>
2021-03-08 23:49:23 -0700

    adding get order method

--- InventoryUpdator.java
+++ InventoryUpdator.java
@@ -7,6 +7,9 @@
  */
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 import java.util.ArrayList;
 
@@ -44,6 +47,146 @@ public class InventoryUpdator {
     public void makeOrder(){
 
     }
+
+    private OrderItem GetOrder(){
+        // Variables
+        String date = null, cust_email = null, cust_location = null, product_id = null;
+        int quantity = 0;
+
+        // Date-Time Format "YYYY-MM-DD"
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+        boolean date_Valid = false;
+
+        // Separator for user readability
+        String s = "----------------------------------------"; // separator
+
+        boolean user_confirmed = false;
+        while (!user_confirmed) {
+
+            // module header
+            System.out.println(s);
+            System.out.println("Adding Order: ");
+            System.out.println(s);
+
+            // Getting the user date for entry
+            System.out.print("Enter Date(YYYY-MM-DD): ");
+            date = in.next();
+            //This while loop will check if the date is valid
+            while (!date_Valid) {
+                try {
+                    LocalDate.parse(date, dateFormatter);
+                    System.out.println("Validated Date");
+                    date_Valid = true;
+                } catch (DateTimeParseException e) {
+                    date_Valid =  false;
+                    System.out.println("Invalid Date");
+                    System.out.print("Enter valid date(YYYY-MM-DD):");
+                    date = in.next();
+                }
+            }
+
+            // Getting user email
+            System.out.print("Enter customer email: ");
+            cust_email = in.next();
+            boolean flag = false;
+            int countr = 0, countd = 0;
+            while(!flag) {
+                //This loop will check if the email is valid
+                for (int i = 0; i < cust_email.length(); i++) {
+                    if (cust_email.charAt(i) == '@') {
+                        countr++;
+                        if (countr > 1) {
+                            flag = false;
+                            break;
+                        }
+                        if (i >= 1) flag = true;
+                        else {
+                            flag = false;
+                            break;
+                        }
+
+                    }
+                    if (cust_email.charAt(i) == '.') {
+                        countd++;
+                        if (countd > 1) {
+                            flag = false;
+                            break;
+                        }
+                        if (i >= 3) flag = true;
+                        else {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (cust_email.indexOf(".") - cust_email.indexOf("@") >= 2) {
+                        flag = true;
+                    }
+                    if (!flag) break;
+                }
+                if (flag && cust_email.length() >= 5) {
+                    System.out.println("Validated Email");
+                    break;
+                } else {
+                    System.out.println("Invalid Email");
+                }
+            }
+
+            //Validate the customer ZIP code
+            System.out.print("Enter ZIP Code: ");
+            cust_location = in.next();
+            while((cust_location.length()) != 5){
+                System.out.println("ZIP Code must be 5 characters long");
+                System.out.print("Enter ZIP Code: ");
+                cust_location = in.next();
+            }
+
+            // Validate product id
+            System.out.print("Enter Product ID: ");
+            product_id = in.next();
+            while ((product_id.length()) != 12) {
+                System.out.println("Product ID must be 12 characters long!");
+                System.out.print("Enter Product ID: ");
+                product_id = in.next();
+            }
+
+            // Validate quantity
+            System.out.print("Enter Quantity: ");
+            while (!in.hasNextInt()) {
+                System.out.println("Quantity must be a whole number!");
+                System.out.print("Enter Quantity: ");
+                in.next();
+            }
+            quantity = in.nextInt();
+
+            //Confirming Entries
+            System.out.println(s);
+            System.out.println("You entered the following values:");
+            System.out.println(s);
+            System.out.printf("%-11s %-20s %-20s  %-18s %-11s\n", "|DATE:|", "|CUSTOMER EMAIL:|", "|CUSTOMER LOCATION:|", "|PRODUCT ID:|", "|QUANTITY:|");
+            System.out.printf("%-11s %-20s %-20s  %-18s %11s\n", date, cust_email, cust_location, product_id, quantity);
+            System.out.println(s);
+            System.out.println("Is this correct?");
+            System.out.print("Type 'yes' to add this record, type 'no' to start over: ");
+            String inp = in.next();
+            boolean validated = false;
+            while (validated == false) {
+                if (inp.toLowerCase().equals("yes")) {
+                    validated = true;
+                    user_confirmed = true;
+                }
+                else if (inp.toLowerCase().equals("no")) {
+                    validated = true;
+
+                }
+                else {
+                    System.out.print("Invalid response. Please type 'yes' or 'no': ");
+                    inp = in.next();
+                }
+            }
+        }
+        return new OrderItem(date, cust_email, cust_location, product_id, quantity);
+    }
+
     //This will check to see if there is a product with the same ID
     public boolean validateProductID( String product_id){
         item = dataBase.viewRecord(product_id);
