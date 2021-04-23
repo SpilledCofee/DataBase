46686a071b985239e92f7e4abaa62ce844ca9315
Josh <77811085+jtwardosz1@users.noreply.github.com>
2021-04-23 06:25:22 -0600

    Update to customer_order_db_remote
    DO NOT MERGE YET
    
    ADD and SEARCH functions are working with new tables.
    
    TODO:
    UPDATE function
    DELETE function

--- mySqlDatabaseRemote/src/mySqlDriver2/cust_order_db_remote.java
+++ mySqlDatabaseRemote/src/mySqlDriver2/cust_order_db_remote.java
@@ -14,19 +14,27 @@ import java.util.Scanner;
 import java.util.Date;
 
 public class cust_order_db_remote {
-
 	
-	public static String url = "";
-	public static String username = "";
-	public static String password = "";
+	private static String url = "";
+	private static String username = "";
+	private static String password = "";
 
 	private Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		cust_order_db_remote drive = new cust_order_db_remote();
+
+		Scanner log = new Scanner(System.in);
+
+		System.out.println("Enter your Username: ");
+		username = log.next();
+
+		System.out.println("Enter your Password: ");
+		password = log.next();
+
 		drive.displayMenu();
-		
 	}
+
 	public void displayMenu(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Select a prompt: ");
@@ -47,7 +55,6 @@ public class cust_order_db_remote {
 		if(prompt.contains("c")) {
 			updateOrder();
 		}
-		
 		if(prompt.contains("d")) {
 			deleteOrder();
 		}
@@ -71,16 +78,15 @@ public class cust_order_db_remote {
 			if (myRs.next()) {
 
 				myRs.beforeFirst();
-	
+
 				while (myRs.next()) {
 					System.out.println();
 					System.out.println("Order ID: " + myRs.getInt("order_id"));
 					System.out.println("Date: " + myRs.getString("ordered_at"));
 					System.out.println("Status: " + myRs.getString("order_status"));
 					System.out.println("Email: " + myRs.getString("user_id"));
-					System.out.println("Product ID: " + myRs.getString("product_id"));
 					System.out.println("Quantity: " + myRs.getInt("order_quantity"));
-					System.out.println("Quantity: " + myRs.getDouble("order_total"));
+					System.out.println("Order Total: " + myRs.getDouble("order_total"));
 					System.out.println("Street: " + myRs.getString("shipping_street"));
 					System.out.println("City: " + myRs.getString("shipping_city"));
 					System.out.println("Street: " + myRs.getString("shipping_state"));
@@ -92,6 +98,8 @@ public class cust_order_db_remote {
             System.out.println("Sorry, email was not found.");
             System.out.println();
 		}
+
+
 		}catch (SQLException e) {	
 			// TODO Auto-generated catch block
 			System.out.println("oops, error!");
@@ -106,7 +114,7 @@ public class cust_order_db_remote {
 			ResultSet.CONCUR_UPDATABLE);
 		
 			ResultSet myRs = myStmt.executeQuery();
-			if (myRs.next()) {	
+			if (myRs.next()) {
 				
 				myRs.beforeFirst();
 
@@ -116,9 +124,8 @@ public class cust_order_db_remote {
 					System.out.println("Date: " + myRs.getString("ordered_at"));
 					System.out.println("Status: " + myRs.getString("order_status"));
 					System.out.println("Email: " + myRs.getString("user_id"));
-					System.out.println("Product ID: " + myRs.getString("product_id"));
 					System.out.println("Quantity: " + myRs.getInt("order_quantity"));
-					System.out.println("Quantity: " + myRs.getDouble("order_total"));
+					System.out.println("Order total: " + myRs.getDouble("order_total"));
 					System.out.println("Street: " + myRs.getString("shipping_street"));
 					System.out.println("City: " + myRs.getString("shipping_city"));
 					System.out.println("Street: " + myRs.getString("shipping_state"));
@@ -129,7 +136,7 @@ public class cust_order_db_remote {
 			else{
 				System.out.println("Sorry no orders with that zipcode was found");
 				System.out.println();
-			}
+				}
 		}catch (SQLException e) {
 			
 			// TODO Auto-generated catch block
@@ -141,7 +148,7 @@ public class cust_order_db_remote {
 	public void searchProcuctID(String pid){
 		try {
 			Connection connection = DriverManager.getConnection(url, username, password);
-			PreparedStatement myStmt = connection.prepareStatement("Select * FROM new_customer_orders WHERE product_id = '" + pid + "'", ResultSet.TYPE_SCROLL_SENSITIVE, 
+			PreparedStatement myStmt = connection.prepareStatement("Select * FROM new_order_items WHERE product_id = '" + pid + "'", ResultSet.TYPE_SCROLL_SENSITIVE, 
 			ResultSet.CONCUR_UPDATABLE);
 
 			ResultSet myRs = myStmt.executeQuery();
@@ -153,16 +160,13 @@ public class cust_order_db_remote {
 				while (myRs.next()) {
 					System.out.println();
 					System.out.println("Order ID: " + myRs.getInt("order_id"));
-					System.out.println("Date: " + myRs.getString("ordered_at"));
-					System.out.println("Status: " + myRs.getString("order_status"));
 					System.out.println("Email: " + myRs.getString("user_id"));
-					System.out.println("Product ID: " + myRs.getString("product_id"));
-					System.out.println("Quantity: " + myRs.getInt("order_quantity"));
-					System.out.println("Quantity: " + myRs.getDouble("order_total"));
-					System.out.println("Street: " + myRs.getString("shipping_street"));
-					System.out.println("City: " + myRs.getString("shipping_city"));
-					System.out.println("Street: " + myRs.getString("shipping_state"));
-					System.out.println("Zip Code: " + myRs.getString("shipping_zipcode"));
+					System.out.println("Email: " + myRs.getString("product_id"));
+					System.out.println("Quantity: " + myRs.getInt("Quantity"));
+					System.out.println("Sale Price: " + myRs.getDouble("sale_price"));
+					System.out.println("Item total: " + myRs.getDouble("item_total"));
+					System.out.println("Item Sale ID: " + myRs.getString("id"));
+
 					System.out.println();
 				}
 			}
@@ -197,9 +201,8 @@ public class cust_order_db_remote {
 					System.out.println("Date: " + myRs.getString("ordered_at"));
 					System.out.println("Status: " + myRs.getString("order_status"));
 					System.out.println("Email: " + myRs.getString("user_id"));
-					System.out.println("Product ID: " + myRs.getString("product_id"));
 					System.out.println("Quantity: " + myRs.getInt("order_quantity"));
-					System.out.println("Quantity: " + myRs.getDouble("order_total"));
+					System.out.println("Order total: " + myRs.getDouble("order_total"));
 					System.out.println("Street: " + myRs.getString("shipping_street"));
 					System.out.println("City: " + myRs.getString("shipping_city"));
 					System.out.println("Street: " + myRs.getString("shipping_state"));
@@ -235,9 +238,8 @@ public class cust_order_db_remote {
 				System.out.println("Date: " + myRs.getString("ordered_at"));
 				System.out.println("Status: " + myRs.getString("order_status"));
 				System.out.println("Email: " + myRs.getString("user_id"));
-				System.out.println("Product ID: " + myRs.getString("product_id"));
 				System.out.println("Quantity: " + myRs.getInt("order_quantity"));
-				System.out.println("Quantity: " + myRs.getDouble("order_total"));
+				System.out.println("Order Total: " + myRs.getDouble("order_total"));
 				System.out.println("Street: " + myRs.getString("shipping_street"));
 				System.out.println("City: " + myRs.getString("shipping_city"));
 				System.out.println("Street: " + myRs.getString("shipping_state"));
@@ -246,7 +248,7 @@ public class cust_order_db_remote {
 			
 			}
 		else{
-            System.out.println("Sorry, no oder with that ID was found.");
+            System.out.println("Sorry, no order with that ID was found.");
             System.out.println();
 		}
 		}catch (SQLException e) {	
@@ -308,7 +310,18 @@ public class cust_order_db_remote {
 				searchOrderID(oid);
 				
 			}
-			
+		System.out.println("Would you like to see more details of the order?\n" 
+			+ "Yes or no?\n");
+		String answer1 = in.nextLine();
+		if(answer1.contains("y") || answer1.contains("Y")){
+			System.out.println("Enter the order id of the order you would like to view: ");
+			int oid2 = scanner.nextInt();
+			searchProducts(oid2);
+		}
+		else{
+			displayMenu();
+		}
+
 		System.out.println("Would you like to make another search?\n" 
 			+ "Yes or no?\n");
 		String answer = in.nextLine();
@@ -320,6 +333,45 @@ public class cust_order_db_remote {
 		}
 	
 	}
+
+		public void searchProducts(int oid){
+			try {
+				Connection connection = DriverManager.getConnection(url, username, password);
+				
+				PreparedStatement myStmt = connection.prepareStatement("SELECT * FROM new_order_items WHERE order_id = '" + oid + "'", ResultSet.TYPE_SCROLL_SENSITIVE, 
+				ResultSet.CONCUR_UPDATABLE);
+			
+				ResultSet myRs = myStmt.executeQuery();
+				if (myRs.next()) {	
+					
+					myRs.beforeFirst();
+					System.out.println();
+					System.out.println("ITEMS ORDERED");
+					System.out.println("----------------");
+					System.out.println();
+					while (myRs.next()) {
+						System.out.println();
+						System.out.println("Order ID: " + myRs.getInt("order_id"));
+						System.out.println("Email: " + myRs.getString("user_id"));
+						System.out.println("Product ID: " + myRs.getString("product_id"));
+						System.out.println("Quantity: " + myRs.getInt("quantity"));
+						System.out.println("Sale Price: " + myRs.getDouble("sale_price"));
+						System.out.println("Item Total: " + myRs.getDouble("item_total"));
+						System.out.println();
+					}
+				
+				
+				}
+			else{
+				System.out.println("Sorry, no oder with that ID was found.");
+				System.out.println();
+			}
+			}catch (SQLException e) {	
+			// TODO Auto-generated catch block
+			System.out.println("oops, error!");
+			e.printStackTrace();
+			}
+		}
 		public void addOrder() {
 
 			Scanner scanner = new Scanner(System.in);
@@ -330,15 +382,11 @@ public class cust_order_db_remote {
 				
 				connection = DriverManager.getConnection(url, username, password);
 				
-				String query = " INSERT INTO new_customer_orders (ordered_at, order_status, user_id, product_id, order_quantity, order_total, shipping_street, shipping_city, shipping_state, shipping_zipcode)" +
-						" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				String query = " INSERT INTO new_customer_orders (ordered_at, user_id, shipping_street, shipping_city, shipping_state, shipping_zipcode)" +
+						" VALUES (?, ?, ?, ?, ?, ?)";
 
-
-				System.out.println();
-				System.out.println();
-				System.out.println("Enter order status: ");
-				String status = scanner.next();
-				System.out.println();
+				String iquery = " INSERT INTO new_order_items (order_id, user_id, product_id, quantity, sale_price, item_total) VALUES (?, ?, ?, ?, ?, ?)";
+				
 
 				System.out.println();
 				System.out.println();
@@ -346,71 +394,135 @@ public class cust_order_db_remote {
 				String userId = scanner.next();
 				System.out.println();
 				
+				scanner.nextLine();
 				System.out.println();
-				System.out.println("Enter the product id: ");
-				int productId = scanner.nextInt();
-				System.out.println();
-
-				System.out.println();
-				System.out.println("Enter the quantity: ");
-				int quantity = scanner.nextInt();
+				System.out.println("Enter the customer street address: ");
+				String street = scanner.nextLine();
 				System.out.println();
 
-				PreparedStatement myStmt3 = connection.prepareStatement("Select sale_price FROM new_inventory WHERE product_id = '" + productId + "'");
-				ResultSet myRs = myStmt3.executeQuery();
-				
-				//Should this be something that is auto calculated with the quantity from the frontend or inventory?
-				if(myRs.next()){
-					double price = myRs.getDouble("sale_price");
-					
-					double total = quantity * price; 
+				System.out.println();
+				System.out.println("Enter the customer City: ");
+				String city = scanner.nextLine();
+				System.out.println();
 
-					scanner.nextLine();
-					System.out.println();
-					System.out.println("Enter the customer street address: ");
-					String street = scanner.nextLine();
-					System.out.println();
+				System.out.println();
+				System.out.println("Enter the customer State(abbreviation only ex: CO for Colorado): ");
+				String state = scanner.next();
+				System.out.println();
 
-					System.out.println();
-					System.out.println("Enter the customer City: ");
-					String city = scanner.nextLine();
-					System.out.println();
+				System.out.println();
+				System.out.println("Enter customer zip code: ");
+				String custZip = scanner.next();
 
-					System.out.println();
-					System.out.println("Enter the customer State(abbreviation only ex: CO for Colorado): ");
-					String state = scanner.next();
-					System.out.println();
-
-					System.out.println();
-					System.out.println("Enter customer zip code: ");
-					String custZip = scanner.next();
-					while(custZip.length() != 5){
-						System.out.println("Invalid zip, try again: ");
-						custZip = scanner.next();
-					}
-				
-					System.out.println();
-				
-					Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-				
-
-					PreparedStatement prepStmt = connection.prepareStatement(query);
-					prepStmt.setTimestamp(1, timestamp);
-					prepStmt.setString (2, status);
-					prepStmt.setString (3, userId);
-					prepStmt.setInt (4, productId);
-					prepStmt.setInt (5, quantity);
-					prepStmt.setDouble(6, total);
-					prepStmt.setString(7, street);
-					prepStmt.setString(8, city);
-					prepStmt.setString(9, state);
-					prepStmt.setString(10, custZip);
-
-		
-					
-					prepStmt.execute();
+				while(custZip.length() != 5){
+					System.out.println("Invalid zip, try again: ");
+					custZip = scanner.next();
 				}
 
+				System.out.println();
+				Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			
+
+				PreparedStatement prepStmt = connection.prepareStatement(query);
+				prepStmt.setTimestamp(1, timestamp);
+				prepStmt.setString (2, userId);
+				prepStmt.setString(3, street);
+				prepStmt.setString(4, city);
+				prepStmt.setString(5, state);
+				prepStmt.setString(6, custZip);
+
+				prepStmt.execute();
+
+
+				
+				int cont = 0;
+				while(cont == 0){
+					System.out.println();
+					System.out.println();
+					System.out.println("Enter the product id: ");
+					int productId = scanner.nextInt();
+					System.out.println();
+
+					System.out.println();
+					System.out.println();
+					System.out.println("Enter the quantity: ");
+					int quantity = scanner.nextInt();
+					System.out.println();
+
+					PreparedStatement myStmt3 = connection.prepareStatement("Select sale_price FROM new_inventory WHERE product_id = '" + productId + "'");
+
+					ResultSet myRs = myStmt3.executeQuery();
+
+					PreparedStatement prepStmt2 = connection.prepareStatement("SELECT order_id FROM new_customer_orders ORDER BY order_id DESC LIMIT 1");
+
+					ResultSet myRs1 = prepStmt2.executeQuery();
+
+					
+					
+					
+					if (myRs1.next()){
+						int oid = myRs1.getInt("order_id");
+
+						if(myRs.next()){
+							double price = myRs.getDouble("sale_price");
+							double total = quantity * price; 
+	
+						
+						PreparedStatement prepStmt1 = connection.prepareStatement(iquery);
+						prepStmt1.setInt(1, oid);
+						prepStmt1.setString (2, userId);
+						prepStmt1.setInt(3, productId);
+						prepStmt1.setInt (4, quantity);
+						prepStmt1.setDouble(5, price);
+						prepStmt1.setDouble(6, total);
+						
+
+						prepStmt1.execute();
+						
+						PreparedStatement myStmt6 = connection.prepareStatement("SELECT SUM(quantity) total_quantity FROM new_order_items WHERE order_id = '" + oid + "'");
+
+						ResultSet myRs2 = myStmt6.executeQuery();
+						if(myRs2.next()){
+
+							int item_total = myRs2.getInt("total_quantity");
+							PreparedStatement myStmt7 = connection.prepareStatement("SELECT SUM(item_total) total_cost FROM new_order_items WHERE order_id = '" + oid + "'");
+							ResultSet myRs3 = myStmt7.executeQuery();
+							if(myRs3.next()){
+
+								
+								double order_total = myRs3.getDouble("total_cost");
+								PreparedStatement prepStmt3 = connection.prepareStatement("UPDATE new_customer_orders SET order_total = '"+ order_total +"' WHERE order_id = '"+ oid +"'");
+								prepStmt3.execute();
+								PreparedStatement prepStmt4 = connection.prepareStatement("UPDATE new_customer_orders SET order_quantity = '"+ item_total +"' WHERE order_id = '"+ oid +"'");
+								prepStmt4.execute();
+
+						}}
+			
+					}
+				}
+
+			
+					System.out.println("Would you like to add another product?");
+				String more = scanner.next();
+					
+				if(more.contains("n")){
+					cont = cont +1;
+					}
+				
+				
+				
+				
+				//PreparedStatement myStmt3 = connection.prepareStatement("Select sale_price FROM new_inventory WHERE product_id = '" + productId + "'");
+				//ResultSet myRs = myStmt3.executeQuery();
+				
+				//Should this be something that is auto calculated with the quantity from the frontend or inventory?
+				//if(myRs.next()){
+
+					//double price = myRs.getDouble("sale_price");
+					//double total = quantity * price; 
+
+				
+				}
 				
 			} catch (SQLException se) {
 				
@@ -494,9 +606,8 @@ public class cust_order_db_remote {
 		System.out.println("Which field would you like to update? ");
 		System.out.println("a. Date");
 		System.out.println("b. Shipping Information");
-		System.out.println("c. Product ID");
-		System.out.println("d. Quantity");
-		System.out.println("e. Email");
+		System.out.println("c. Order Details");
+		System.out.println("d. Email");
 		System.out.println();
 					
 		String ans = scanner.next();
@@ -528,7 +639,7 @@ public class cust_order_db_remote {
 			System.out.println();
 			String street = scanner.nextLine();
 					
-			PreparedStatement upStmt3 = connection.prepareStatement("UPDATE new_customer_orders SET shipping_street = '" + street +"' WHERE order_id = '"+ oid +"'");
+			PreparedStatement upStmt3 = connection.prepareStatement("UPDATE new_customer_orders SET shipping_street = '" + street + "' WHERE order_id = '"+ oid +"'");
 			upStmt3.execute();
 
 			System.out.println();
@@ -549,20 +660,18 @@ public class cust_order_db_remote {
 						
 		}
 					
+							
 		if(ans.contains("c")) {
 						
+
 			System.out.println();
 			System.out.println("Enter the new Product ID: ");
 			System.out.println();
 			String pid = scanner.next();
 						
-			PreparedStatement upStmt2 = connection.prepareStatement("UPDATE new_customer_orders SET product_id = '" + pid +"' WHERE order_id = '"+ oid +"'");
-			upStmt2.execute();
-						
-		}
-							
-		if(ans.contains("d")) {
-						
+			PreparedStatement upStmt3 = connection.prepareStatement("UPDATE new_customer_orders SET product_id = '" + pid +"' WHERE order_id = '"+ oid +"'");
+			upStmt3.execute();
+
 			System.out.println();
 			System.out.println("Enter the new quantity: ");
 			System.out.println();
@@ -572,7 +681,7 @@ public class cust_order_db_remote {
 			upStmt2.execute();
 						
 		}
-		if(ans.contains("e")) {
+		if(ans.contains("d")) {
 						
 			System.out.println();
 			System.out.println("Enter the new email: ");
