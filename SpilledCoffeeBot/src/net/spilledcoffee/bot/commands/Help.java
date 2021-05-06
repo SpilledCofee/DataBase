c25ad49c47d92f544a3252b3a96be30cb85af5b1
jcasta30 <54787646+jcasta30@users.noreply.github.com>
2021-05-05 20:23:09 -0600

    Updated_Discord-Bot_V2
    -Removed unnecessary classes not incorporated
    -Added AllProducts command
    -Updated the main, help, and private message classes

--- SpilledCoffeeBot/src/net/spilledcoffee/bot/commands/Help.java
+++ SpilledCoffeeBot/src/net/spilledcoffee/bot/commands/Help.java
@@ -22,7 +22,7 @@ public class Help extends ListenerAdapter {
             info.setDescription("Joining this server allows you to enter known commands into your chat box, then our bot will help you " +
                     "execute some of those commands. This server is dedicated to fulfill some of our customers needs at the convenience of Discord." +
                     "\nBelow are accepted commands: ");
-            info.addField("Commands", "-help\n-recommend\n-pastorders\n-productsavailable\n", false);
+            info.addField("Commands", "-help\n-recommend\n-pastorders\n-productsavailable\n-allproducts\n", false);
             info.addField("Joining", "When you join the server our bot will immediately send you a private direct message, that is where you will enter your desired commands!\n", false);
             info.addField("Extra Notes: ", "For the bot to recognize a command you must enter a '-' in front of the command!\n\nIf you need specific information on how to use a specific command, " +
                     "enter in your chat the help command along with the desired command. \nFor example: '-help [-Command]' (without the brackets) ", false);
@@ -46,7 +46,9 @@ public class Help extends ListenerAdapter {
         // If user enters -help, along with the command productsavailable,
         // it will output information regarding that specific command.
         else if (args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
-                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "productsavailable")) {
+                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "productsavailable") ||
+                args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
+                        && args[1].equalsIgnoreCase( "productsavailable")) {
             // Create an embed with information regarding the productsavailable command.
             EmbedBuilder helpAvailable = new EmbedBuilder();
             helpAvailable.setTitle("Products Available Command Information:");
@@ -65,7 +67,9 @@ public class Help extends ListenerAdapter {
         // If user enters -help, along with the command pastorders,
         // it will output information on how to use that specific command
         else if (args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
-                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "pastorders")) {
+                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "pastorders") ||
+                args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
+                        && args[1].equalsIgnoreCase( "pastorders")) {
             // Create an embed with information regarding the pastorders command.
             EmbedBuilder helpPastOrders = new EmbedBuilder();
             helpPastOrders.setTitle("Past Orders Command Information:");
@@ -83,7 +87,9 @@ public class Help extends ListenerAdapter {
         // If user enters -help, along with the recommend command,
         // it will output information on how to use that specific command
         else if (args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
-                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "recommend")) {
+                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "recommend") ||
+                args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
+                        && args[1].equalsIgnoreCase( "recommend")) {
             // Create an embed with information regarding the recommend command.
             EmbedBuilder helpRecommend = new EmbedBuilder();
             helpRecommend.setTitle("Recommend Command Information:");
@@ -97,6 +103,27 @@ public class Help extends ListenerAdapter {
             event.getChannel().sendMessage(helpRecommend.build()).queue();
             // embed clear
             helpRecommend.clear();
+        }
+
+        // If user enters -help, along with the allproducts command,
+        // it will output information on how to use that specific command
+        else if (args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
+                && args[1].equalsIgnoreCase(CoffeeBot.prefix + "allproducts") ||
+                args[0].equalsIgnoreCase(CoffeeBot.prefix + "help")
+                && args[1].equalsIgnoreCase( "allproducts")) {
+            // Create an embed with information regarding the recommend command.
+            EmbedBuilder helpAllPros = new EmbedBuilder();
+            helpAllPros.setTitle("All Products Command Information:");
+            helpAllPros.setDescription("The allproducts command simply asks the Spilled coffee bot " +
+                    "to return all of the current products available at Spilled Coffee." +
+                    "To apply the command, simply enter the allproducts command.");
+            helpAllPros.addField("Format:", "-allproducts", false);
+            helpAllPros.addField("Note:", "For a better view at our products please visit our website:\nSpilledcoffee.net", false);
+            helpAllPros.setColor(0xffa640);
+            // send embed message
+            event.getChannel().sendMessage(helpAllPros.build()).queue();
+            // embed clear
+            helpAllPros.clear();
         }
     }
 }
